6e6372b03c7e55a9541d4eeba308b96caf7a1820
ariesgate <134568565+ariesgate@users.noreply.github.com>
2023-12-11 02:40:46 -0500

    Create Day5.sol

---
+++ Day5.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+contract day5{
+
+    string[] public alphabet  = ["A","B","C"];
+
+      function addToEnd(string calldata all) public{
+        alphabet.push(all);
+        //push puts value at the end
+
+      }
+
+      function removeEnd() public{
+        //pop gets rid of the value at the end
+
+        alphabet.pop();
+      }
+
+}
